f7d5d3957425763050a64a7f2bd764b3a88810f0
edwincanopy <edwin@canopylabs.ai>
2024-06-29 14:27:02 +0100

    Update index.js

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -53,7 +53,7 @@ console.log('Completed setup.');
 // Load the .glb file
 const loader = new GLTFLoader();
 loader.load(
-    './public/models/blender_bernice_full_rig_demo.glb', // URL to the .glb file
+    '../public/models/blender_bernice_full_rig_demo.glb', // URL to the .glb file
     function (gltf) {
         scene.add(gltf.scene);
         animate();
